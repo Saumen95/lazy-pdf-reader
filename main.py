70785a91b6b7036db2b5ac2fae144c5a4190f635
Saumen95 <saumenroy323@gmail.com>
2020-10-07 02:09:15 +0600

    properties changed

--- main.py
+++ main.py
@@ -5,10 +5,17 @@ book = open('python.pdf', 'rb')
 pdfReader = PyPDF2.PdfFileReader(book)
 pages = pdfReader.numPages
 speaker = pyttsx3.init()
+
+# changing some properties
+rate = speaker.getProperty('rate')
+print(rate)
+speaker.setProperty('rate', 125) # speed
+voices = speaker.getProperty('voices')
+speaker.setProperty('voice', voices[1].id) #female voice
 for num in range(0, pages):
     page = pdfReader.getPage(0)
     text = page.extractText()
     speaker.say(text)
     speaker.runAndWait()
-    
+
 
